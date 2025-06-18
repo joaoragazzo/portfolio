8da56aad64a8dba194b8f3682830829344bad585
joaoragazzo <joao_ragazzo@hotmail.com>
2025-06-17 23:30:42 -0300

    testing ci/cd

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,6 +1,9 @@
 function App() {
   return (
-    <p>Hello, world!!</p>
+    <>
+      <p className="bg-red-100 p-2 text-red-700">Hello, world!</p>
+      <p className="bg-green-100 p-2 text-green-700">It's working!</p>  
+    </>
   )
 }
 
