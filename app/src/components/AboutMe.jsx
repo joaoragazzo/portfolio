4e0703cae217181bf16367d1bec7158bee9fa5d2
joaoragazzo <joao_ragazzo@hotmail.com>
2025-06-19 19:34:16 -0300

    feat: add a profile picutre

--- app/src/components/AboutMe.jsx
+++ app/src/components/AboutMe.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import picutre from "../../public/profile_picture.jpeg";
 
 export const AboutMe = ({ children }) => {
   const childrenArray = React.Children.toArray(children);
@@ -14,20 +15,7 @@ export const AboutMe = ({ children }) => {
       <div className="w-full md:w-1/3 flex justify-center">
         <div className="w-64 h-64 rounded-full bg-gradient-to-br from-sky-600 to-indigo-800 p-1">
           <div className="w-full h-full rounded-full bg-slate-900 flex items-center justify-center text-6xl">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="w-32 h-32 text-sky-400"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="1"
-                d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
-              />
-            </svg>
+            <img className="w-full h-full object-cover rounded-full brightness-90" src={picutre} />
           </div>
         </div>
       </div>
