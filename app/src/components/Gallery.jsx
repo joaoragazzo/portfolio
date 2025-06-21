79c87aeca3fbb2016ff9fdf65197bed420244681
joaoragazzo <joao_ragazzo@hotmail.com>
2025-06-21 18:25:58 -0300

    refact: remove useless import

--- app/src/components/Gallery.jsx
+++ app/src/components/Gallery.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import LightGallery from "lightgallery/react";
-import jsimples from "/public/jsimples.png";
 import "lightgallery/css/lightgallery.css";
 import "lightgallery/css/lg-zoom.css";
 import "lightgallery/css/lg-thumbnail.css";
