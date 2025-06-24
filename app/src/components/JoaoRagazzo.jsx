0d717c4422d3c0b683677f900f1ea1c8dc48c7ec
joaoragazzo <joao_ragazzo@hotmail.com>
2025-06-23 21:26:44 -0300

    feat: add hiperlink to the name

--- app/src/components/JoaoRagazzo.jsx
+++ app/src/components/JoaoRagazzo.jsx
@@ -1,11 +1,13 @@
 export const JoaoRagazzo = () => {
   return (
     <div className="flex items-center mb-0">
-      <div className="code-font text-xl font-semibold text-sky-400">
-        <span className="text-slate-400">&lt;</span>
-        JoaoRagazzo
-        <span className="text-slate-400">/&gt;</span>
-      </div>
+      <a href="#">
+        <div className="code-font text-xl font-semibold text-sky-400">
+          <span className="text-slate-400">&lt;</span>
+          JoaoRagazzo
+          <span className="text-slate-400">/&gt;</span>
+        </div>
+      </a>
     </div>
   );
 };
